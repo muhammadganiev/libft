ca9d5e753690e9e907fb6cf18c3fcef6e3257aa3
muhammadganiev <mukhammad.ganiev@mail.ru>
2022-05-25 19:07:32 +0400

    memset and memmove

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,23 @@
 /*   By: muganiev <gf.black.tv@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 14:40:28 by muganiev          #+#    #+#             */
-/*   Updated: 2022/05/16 14:40:29 by muganiev         ###   ########.fr       */
+/*   Updated: 2022/05/25 19:07:11 by muganiev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t	i;
+
+	if (!b)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		*(unsigned char *)(b + i) = (unsigned char)c;
+		i++;
+	}
+	return (b);
+}
