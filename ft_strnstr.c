846b95d696a6cf5bdedace4841642dd43ecd6974
muhammadganiev <mukhammad.ganiev@mail.ru>
2022-05-25 18:54:43 +0400

    ft_strlcat and ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,31 @@
 /*   By: muganiev <gf.black.tv@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 14:40:45 by muganiev          #+#    #+#             */
-/*   Updated: 2022/05/16 14:40:46 by muganiev         ###   ########.fr       */
+/*   Updated: 2022/05/25 18:43:55 by muganiev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (!haystack || !needle)
+		retur (NULL);
+	if (!needle || !needle[0])
+		return ((char *)haystack);
+	i = 0;
+	while (haystack[i] && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] && needle[j] && i + j < len && haystack[i + j]
+			== needle[j])
+			j++;
+		if (needle[j])
+			return ((char *)(haystack + i));
+		i++;
+	}
+	return (NULL);
+}
