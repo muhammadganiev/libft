797fd46dd0dc2ef0fa201deaef6ab7cf51022ec4
muhammadganiev <mukhammad.ganiev@mail.ru>
2022-05-25 19:28:47 +0400

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,33 @@
 /*   By: muganiev <gf.black.tv@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 14:39:48 by muganiev          #+#    #+#             */
-/*   Updated: 2022/05/23 20:51:04 by muganiev         ###   ########.fr       */
+/*   Updated: 2022/05/25 19:28:35 by muganiev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	is_neg;
+	int	res;
+
+	is_neg = 1;
+	if (!str)
+		return (0);
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || str[i] == '\f'
+		|| str[i] == '\r' || str[i] == ' ')
+		i++;
+	if (str[i] == '-')
+	{
+		is_neg = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	res = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+		res = (res * 10) + (str[i++] - '0');
+	return (res * is_neg);
+}
