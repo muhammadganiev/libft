57ede44b73963106aa5a33ca34c0cc46aa51eb07
muhammadganiev <mukhammad.ganiev@mail.ru>
2022-05-29 21:47:19 +0400

    strjoin done

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,34 @@
 /*   By: muganiev <gf.black.tv@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 15:14:11 by muganiev          #+#    #+#             */
-/*   Updated: 2022/05/26 15:14:12 by muganiev         ###   ########.fr       */
+/*   Updated: 2022/05/29 21:46:40 by muganiev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	str = (char *)malloc(sizeof(s1) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	while (s1[i])
+	{
+		str[j++] = s1[i]
+			i++;
+	}
+	i = 0;
+	while (s2[i])
+	{
+		str[j++] = s2[i]
+			i++;
+	}
+	str[j] = 0;
+	return (str);
+}
