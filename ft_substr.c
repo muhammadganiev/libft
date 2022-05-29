fe4fa2934bf927553c1981f94eda607e6c215bd0
muhammadganiev <mukhammad.ganiev@mail.ru>
2022-05-29 19:32:50 +0400

    substr done

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,32 @@
 /*   By: muganiev <gf.black.tv@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 15:14:25 by muganiev          #+#    #+#             */
-/*   Updated: 2022/05/26 15:14:26 by muganiev         ###   ########.fr       */
+/*   Updated: 2022/05/29 19:32:08 by muganiev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	str = (char *)malloc(sizeof(*s) * (len - 1));
+	if (!str)
+		return (NULL);
+	while (s[i])
+	{
+		if (i > start && j < len)
+		{
+				str[j] = s[i]
+				j++;
+		}
+		i++;
+	}
+	str[j] = 0;
+	return (str);
+}
